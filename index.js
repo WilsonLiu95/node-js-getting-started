08b4f27bd7f3d03a71f7d80ce095d0fa040ea877
wilsonsliu <wilsonliuxyz@gmail.com>
2020-09-19 15:38:23 +0800

    feat: 更新

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path')
+const cool = require('cool-ascii-faces');
+
 const PORT = process.env.PORT || 5000
 console.log('process.env',process.env)
 express()
@@ -7,4 +9,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
